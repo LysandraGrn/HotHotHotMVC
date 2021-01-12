b4fc5e9f4d689f42396ac4b2f5abdd7cf39f60e4
guerinremi1 <49983302+guerinremi1@users.noreply.github.com>
2021-01-12 11:30:13 +0100

    Create profil.php

---
+++ HotHotHotMVC/View/base/profil.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="js/scriptHot.js"></script>
+    <link rel="icon" href="favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/js/all.min.js"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <title>HotHotHot</title>
+
+</head>
+<body>
+<?php View::show('base/header'); ?>
+
+<h2>Profil</h2>
+
+<?php View::show('base/footer'); ?>
+</body>
+
+</html>
